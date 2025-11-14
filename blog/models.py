cf5d9bef688ed7fc990b94ecb760065d54fcb1c5
alanmyszka <github@alanmyszka.pl>
2025-11-14 12:51:47 +0100

    Add Posts and Tags, add UI for Posts

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    url = models.SlugField()
+    description = models.TextField(max_length=1000, null=True)
+    slug = models.SlugField()
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
+    
+    class Meta:
+        ordering = ['-created']
 
     def __str__(self):
         return self.title
+    
+    def excerpt(self, chars=100):
+        if len(self.content) > chars:
+            return self.content[:chars].rstrip() + "…"
+        return self.content
